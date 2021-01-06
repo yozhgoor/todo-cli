4bd71479ea997b81603578615dfa280c220c9685
yozhgoor <yozhgoor@outlook.com>
2021-01-06 18:20:51 +0100

    Impl an insert method who take a reference to the struct and a key and inserting it into our map

--- src/main.rs
+++ src/main.rs
@@ -11,3 +11,11 @@ struct Todo {
     // use rust built in HashMap to store key - val pairs
     map: HashMap<String, bool>,
 }
+
+impl Todo {
+    fn insert(&mut self, key: String) {
+        // insert a new item into our map.
+        // We pass true as a value
+        self.map.insert(key, true);
+    }
+}
