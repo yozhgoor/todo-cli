f7eb186206afe6500ce127e5c480bdd85a1b9d72
yozhgoor <yozhgoor@outlook.com>
2021-01-06 17:34:14 +0100

    Define Todo struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use std::collections::HashMap;
+
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
 
     println!("{:?}, {:?}", action, item);
+}
+
+struct Todo {
+    // use rust built in HashMap to store key - val pairs
+    map: HashMap<String, bool>,
 }
